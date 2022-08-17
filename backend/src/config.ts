d371579cb9f2098bad011ed787cc0f92701a42f8
Lilla Csapo <lantasire@gmail.com>
2022-08-17 14:00:15 +0100

    config.ts created

---
+++ backend/src/config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//export const saltRounds = 10;
+
+export default {
+  mysql: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+};
