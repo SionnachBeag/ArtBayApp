36a3d30ae6df7e7c2dc4aa178f16ea81f499fbd8
Lilla Csapo <lantasire@gmail.com>
2022-08-17 15:09:45 +0100

    errorHandler added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import errorHandler from './middlewares/error-handler';
 import apiRouter from './routes/api.routes';
 
 const app = express();
 
 app.use('/api', apiRouter);
-//app.use(errorHandler);
+app.use(errorHandler);
 app.use(express.static(__dirname + '/uploads/images'));
 
 export default app;
