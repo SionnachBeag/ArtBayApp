585a57c318bd0841611053db8e510f2998ada159
Lilla Csapo <lantasire@gmail.com>
2022-08-18 20:04:47 +0100

    wrote register and login functions

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,19 +1,91 @@
 import { NextFunction, Request, Response } from 'express';
+import { ApiErrorModel } from '../models/ApiErrorModel';
+import { IUserDomainModel } from '../models/domain-models/IUserDomainModel';
 import { ILoginRequestModel } from '../models/request-models/ILoginRequestModel';
 import { IRegisterRequestModel } from '../models/request-models/IRegisterRequestModel';
 import { ILoginViewModel } from '../models/view-models/ILoginViewModel';
 import { IRegisterViewModel } from '../models/view-models/IRegisterViewModel';
+import { emailService } from '../services/email-service';
+import { jwtService } from '../services/JWT-service';
+import { passwordService } from '../services/password-service';
+import { userService } from '../services/user-service';
 
 export const userController = {
   async register(
     req: Request<IRegisterRequestModel>,
     res: Response<IRegisterViewModel>,
     next: NextFunction
-  ) {},
+  ): Promise<void> {
+    const newUser: IRegisterRequestModel = {
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    const isPasswordValid: boolean = passwordService.checkPassword(
+      newUser.password
+    );
+
+    if (!isPasswordValid) {
+      return next({
+        message:
+          'password must be at least 8 characters and contain letter and number',
+        status: 422,
+      });
+    }
+
+    const isEmailValid: boolean = emailService.emailCheck(newUser.email);
+
+    if (!isEmailValid) {
+      return next({
+        message: 'email is invalid',
+        status: 422,
+      });
+    }
+
+    await userService
+      .register(newUser)
+      .then(() => {
+        return res.json({
+          status: 201,
+          message: 'Registration was successful.',
+        });
+      })
+      .catch((err: ApiErrorModel) => {
+        console.log(err);
+        next(err);
+        return;
+      });
+  },
 
   async login(
     req: Request<ILoginRequestModel>,
     res: Response<ILoginViewModel>,
     next: NextFunction
-  ) {},
+  ): Promise<void> {
+    const loginAttempt: ILoginRequestModel = {
+      userName: req.body.userName,
+      password: req.body.password,
+    };
+
+    await userService
+      .login(loginAttempt)
+      .then(async (userData: IUserDomainModel) => {
+        const jwtToken: string = await jwtService.generateAccessToken(
+          userData.id,
+          userData.userName
+        );
+        return res.status(200).json({
+          status: 200,
+          userName: userData.userName,
+          artDollars: userData.artDollars,
+          token: jwtToken,
+        });
+      })
+      .catch((err: ApiErrorModel) => {
+        console.log(err);
+        next(err);
+        return;
+      });
+  },
 };
