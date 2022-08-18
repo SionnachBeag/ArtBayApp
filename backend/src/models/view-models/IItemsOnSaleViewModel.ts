9022bc118358e4d7297c85a01939ec76a7ebafed
Lilla Csapo <lantasire@gmail.com>
2022-08-18 23:29:13 +0100

    IItemsOnSaleViewModel interface created

---
+++ backend/src/models/view-models/IItemsOnSaleViewModel.ts
@@ -0,0 +1,10 @@
+export interface IItemsOnSaleViewModel {
+  id: number;
+  title: string;
+  description: string;
+  imgUrl: string;
+  price: number;
+  isSold: boolean;
+  buyerId?: number;
+  sellerId: number;
+}
