3b024697d5f8ac27c61ab428ced1cabe5466ce6e
Lilla Csapo <lantasire@gmail.com>
2022-08-18 23:33:54 +0100

    added addItemRequestKeys variable

--- backend/src/helpers/request-keys.ts
+++ backend/src/helpers/request-keys.ts
@@ -1,3 +1,11 @@
 export const loginRequestKeys: string[] = ['userName', 'password'];
 
 export const registerRequestKeys: string[] = ['userName', 'email', 'password'];
+
+export const addItemRequestKeys: string[] = [
+  'title',
+  'description',
+  'imgUrl',
+  'price',
+  'sellerId',
+];
