e3ce127ad87a285e695004cca97513afc6c0d259
Lilla Csapo <lantasire@gmail.com>
2022-08-18 23:28:34 +0100

    post request for '/items' route created

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { userController } from '../controllers/user-controller';
 import requestBodyValidator from '../middlewares/request-body-validator';
-import { registerRequestKeys } from '../helpers/request-keys';
-import { loginRequestKeys } from '../helpers/request-keys';
+import {
+  registerRequestKeys,
+  addItemRequestKeys,
+  loginRequestKeys,
+} from '../helpers/request-keys';
+import { itemController } from '../controllers/item-controller';
+import tokenAuthentication from '../middlewares/token-authentication';
 
 const apiRouter = express.Router();
 apiRouter.use(cors());
@@ -16,5 +21,13 @@ apiRouter.post('/register', userController.register);
 
 apiRouter.route('/login').post(requestBodyValidator(loginRequestKeys, 400));
 apiRouter.post('/login', userController.login);
+
+apiRouter
+  .route('/items')
+  .post(requestBodyValidator(addItemRequestKeys, 400), tokenAuthentication());
+apiRouter.post('/items', itemController.addItem);
+
+apiRouter.route('/items').get(tokenAuthentication());
+apiRouter.get('/items', itemController.listAllItemsOnSale);
 
 export default apiRouter;
