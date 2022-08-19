020e58352964235242beeaaf59e1c928a013e3ff
Lilla Csapo <lantasire@gmail.com>
2022-08-19 14:18:20 +0100

    added routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/pages/landing-page/landing-page.module').then(
+        (mod) => mod.LandingPageModule
+      ),
+  },
+  {
+    path: 'shop',
+    loadChildren: () =>
+      import('./modules/pages/shop-page/shop-page.module').then(
+        (mod) => mod.ShopPageModule
+      ),
+  },
+  {
+    path: 'manageItems',
+    loadChildren: () =>
+      import('./modules/pages/manage-items-page/manage-items-page.module').then(
+        (mod) => mod.ManageItemsPageModule
+      ),
+  },
+  {
+    path: 'viewItem',
+    loadChildren: () =>
+      import('./modules/pages/item-page/item-page.module').then(
+        (mod) => mod.ItemPageModule
+      ),
+  },
+  {
+    path: 'pageNotFound',
+    loadChildren: () =>
+      import('./modules/pages/page-not-found/page-not-found.module').then(
+        (mod) => mod.PageNotFoundModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./modules/pages/page-not-found/page-not-found.module').then(
+        (mod) => mod.PageNotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
