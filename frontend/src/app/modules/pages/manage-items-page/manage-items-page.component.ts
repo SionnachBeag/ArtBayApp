a61f313729de491fda19d72bde35898d82738661
Lilla Csapo <lantasire@gmail.com>
2022-08-19 14:11:06 +0100

    manage items page component created

---
+++ frontend/src/app/modules/pages/manage-items-page/manage-items-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-items-page',
+  templateUrl: './manage-items-page.component.html',
+  styleUrls: ['./manage-items-page.component.scss']
+})
+export class ManageItemsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
