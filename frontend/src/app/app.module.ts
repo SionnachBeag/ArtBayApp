b3bec04ec113e243291a334515bea43d1fa90de9
Lilla Csapo <lantasire@gmail.com>
2022-08-19 14:20:21 +0100

    unused imports taken out

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemPageComponent } from './modules/pages/item-page/item-page.component';
-import { ManageItemsPageComponent } from './modules/pages/manage-items-page/manage-items-page.component';
 
 @NgModule({
   declarations: [AppComponent],
