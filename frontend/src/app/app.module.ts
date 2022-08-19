c8c875d28f0e3849a1127b6e4d16ea35aae027dc
Lilla Csapo <lantasire@gmail.com>
2022-08-19 14:19:10 +0100

    only app component left in declarations

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemPageComponent } from './modules/pages/item-page/item-page.component';
+import { ManageItemsPageComponent } from './modules/pages/manage-items-page/manage-items-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
