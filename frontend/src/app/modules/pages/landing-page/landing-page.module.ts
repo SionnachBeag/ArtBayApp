2617fc6422c6413f397323bc37854d039fea79d3
Lilla Csapo <lantasire@gmail.com>
2022-08-20 09:58:56 +0100

    modules added to imports

--- frontend/src/app/modules/pages/landing-page/landing-page.module.ts
+++ frontend/src/app/modules/pages/landing-page/landing-page.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { LoginComponent } from './child-components/login/login.component';
+import { RegisterComponent } from './child-components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingPageRoutingModule],
+  declarations: [LandingPageComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class LandingPageModule {}
