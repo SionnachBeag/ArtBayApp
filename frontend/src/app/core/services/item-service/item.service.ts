1e0150499318eff1dcd12683952a4a3327252e88
Lilla Csapo <lantasire@gmail.com>
2022-08-21 11:08:36 +0100

    item service created

---
+++ frontend/src/app/core/services/item-service/item.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IItemsOnSaleViewModel } from '../../models/IItemsOnSaleViewModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemService {
+  private itemsSubject = new BehaviorSubject<IItemsOnSaleViewModel[]>([]);
+  itemsObservable$: Observable<IItemsOnSaleViewModel[]> =
+    this.itemsSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+  listAllItemsOnSale() {
+    this.http
+      .get<IItemsOnSaleViewModel[]>(`${environment.baseUrl}/items`)
+      .subscribe((items: IItemsOnSaleViewModel[]) => {
+        this.itemsSubject.next(items);
+      });
+  }
+}
