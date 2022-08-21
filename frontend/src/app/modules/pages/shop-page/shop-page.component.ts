5753906309e0a6f3d619068ee5e731e62e1c6ca9
Lilla Csapo <lantasire@gmail.com>
2022-08-21 11:07:25 +0100

    added itemSubsription and ran itemService method, subscribe to Observable + ngOnDestroy

--- frontend/src/app/modules/pages/shop-page/shop-page.component.ts
+++ frontend/src/app/modules/pages/shop-page/shop-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IItemsOnSaleViewModel } from 'src/app/core/models/IItemsOnSaleViewModel';
+import { ItemService } from 'src/app/core/services/item-service/item.service';
 
 @Component({
   selector: 'app-shop-page',
   templateUrl: './shop-page.component.html',
-  styleUrls: ['./shop-page.component.scss']
+  styleUrls: ['./shop-page.component.scss'],
 })
 export class ShopPageComponent implements OnInit {
-
-  constructor() { }
+  itemsSubscription!: Subscription;
+  items: IItemsOnSaleViewModel[] = [];
+  constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
+    this.itemService.listAllItemsOnSale();
+    this.itemsSubscription = this.itemService.itemsObservable$.subscribe(
+      (observableResponse: IItemsOnSaleViewModel[]) => {
+        this.items = observableResponse;
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.itemsSubscription.unsubscribe();
+  }
 }
