f70339e180e502cb682e33f7bb29684985782f7f
Lilla Csapo <lantasire@gmail.com>
2022-08-21 17:19:07 +0100

    added header and footer components to declarations

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [ItemComponent],
-  imports: [CommonModule, MatButtonModule],
-  exports: [ItemComponent],
+  declarations: [ItemComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, MatButtonModule, RouterModule],
+  exports: [ItemComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
