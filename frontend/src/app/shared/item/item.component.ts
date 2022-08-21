c8743996961606bd9228cba9dc07c072a7effdc1
Lilla Csapo <lantasire@gmail.com>
2022-08-21 11:04:34 +0100

    added input itemData to component

--- frontend/src/app/shared/item/item.component.ts
+++ frontend/src/app/shared/item/item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IItemsOnSaleViewModel } from 'src/app/core/models/IItemsOnSaleViewModel';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Input() itemData!: IItemsOnSaleViewModel;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
