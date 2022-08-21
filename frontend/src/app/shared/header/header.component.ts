65c02b8f1e7d4f72e8bac1e0a9c2249bd4d003b9
Lilla Csapo <lantasire@gmail.com>
2022-08-21 17:16:57 +0100

    added logout method to header

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.checkLocalStorageData();
+    this.authService.monitorLocalStorageChanges();
+    if (this.authService.isLoggedIn()) {
+      this.authService.isTokenValid();
+    }
   }
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
