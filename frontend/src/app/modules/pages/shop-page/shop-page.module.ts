11fcee8c7165c8b2eac55f9ca2b712dcd559fb3e
Lilla Csapo <lantasire@gmail.com>
2022-08-21 11:06:00 +0100

    added SharedModule to imports

--- frontend/src/app/modules/pages/shop-page/shop-page.module.ts
+++ frontend/src/app/modules/pages/shop-page/shop-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopPageRoutingModule } from './shop-page-routing.module';
 import { ShopPageComponent } from './shop-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ShopPageComponent],
-  imports: [CommonModule, ShopPageRoutingModule],
+  imports: [CommonModule, ShopPageRoutingModule, SharedModule],
 })
 export class ShopPageModule {}
