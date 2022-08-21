190a1f7b8224c92d9d58da8aaeb8387b3099cb6d
Lilla Csapo <lantasire@gmail.com>
2022-08-21 17:24:12 +0100

    methods and properties added

--- frontend/src/app/modules/pages/item-page/item-page.component.ts
+++ frontend/src/app/modules/pages/item-page/item-page.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IItemsOnSaleViewModel } from 'src/app/core/models/IItemsOnSaleViewModel';
+import { ItemService } from 'src/app/core/services/item-service/item.service';
 
 @Component({
   selector: 'app-item-page',
   templateUrl: './item-page.component.html',
-  styleUrls: ['./item-page.component.scss']
+  styleUrls: ['./item-page.component.scss'],
 })
 export class ItemPageComponent implements OnInit {
+  itemsSubscription!: Subscription;
+  item!: IItemsOnSaleViewModel;
+  id!: number;
 
-  constructor() { }
+  constructor(
+    private itemService: ItemService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  get ImgUrl(): string | null {
+    return this.item && this.item.imgUrl
+      ? `http://localhost:3000/${this.item.imgUrl}`
+      : null;
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      try {
+        this.id = parseInt(params['id']);
+      } catch (err) {
+        this.router.navigateByUrl('/pageNotFound');
+        return;
+      }
+    });
+
+    this.itemService
+      .getItemById(this.id)
+      .subscribe((response: IItemsOnSaleViewModel) => {
+        this.item = response;
+      });
+  }
 }
