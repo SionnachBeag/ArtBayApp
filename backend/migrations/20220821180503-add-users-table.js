67e3eef4e3b054f07e8ccf4b98d1d56b8aac65fe
Lilla Csapo <lantasire@gmail.com>
2022-08-22 10:47:55 +0100

    migration remade

---
+++ backend/migrations/20220821180503-add-users-table.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  db.createTable(
+    'users',
+    {
+      columns: {
+        id: {
+          type: 'int',
+          primaryKey: true,
+          autoIncrement: true,
+          notNull: true,
+        },
+        userName: {
+          type: 'string',
+          unique: true,
+          notNull: true,
+        },
+        email: {
+          type: 'string',
+          unique: true,
+          notNull: true,
+        },
+        password: {
+          type: 'string',
+          notNull: true,
+        },
+        artDollars: {
+          type: 'int',
+          notNull: true,
+          defaultValue: 100,
+        },
+      },
+      ifNotExists: true,
+    },
+    callback
+  );
+};
+
+exports.down = function (db, callback) {
+  db.dropTable('users', callback);
+};
+
+exports._meta = {
+  version: 1,
+};
