f4687d0773d5a784ddee567e9913d7381d4fbd67
Lilla Csapo <lantasire@gmail.com>
2022-08-22 10:49:10 +0100

    IItemAndBuyerModel added

---
+++ backend/src/models/IItemAndBuyerModel.ts
@@ -0,0 +1,10 @@
+export interface IItemAndBuyerModel {
+  id: number;
+  title: string;
+  description: string;
+  imgUrl: string;
+  price: number;
+  isSold: boolean;
+  userName: string;
+  buyerName: string;
+}
