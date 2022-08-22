f15c033c094ce70fd1f128098b5c8d51b7320dc7
Lilla Csapo <lantasire@gmail.com>
2022-08-22 16:44:14 +0100

    form group and onSubmit() added

--- frontend/src/app/modules/pages/manage-items-page/manage-items-page.component.ts
+++ frontend/src/app/modules/pages/manage-items-page/manage-items-page.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ItemService } from 'src/app/core/services/item-service/item.service';
+import { AuthService } from 'src/app/core/services/auth-service/auth.service';
+import { Router } from '@angular/router';
+import { ICreateItem } from 'src/app/core/models/ICreateItem';
+import { IItemFormInput } from 'src/app/core/models/IItemFormInput';
 
 @Component({
   selector: 'app-manage-items-page',
   templateUrl: './manage-items-page.component.html',
-  styleUrls: ['./manage-items-page.component.scss']
+  styleUrls: ['./manage-items-page.component.scss'],
 })
 export class ManageItemsPageComponent implements OnInit {
+  message: string = '';
+  itemForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    imgUrl: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+  });
+  constructor(
+    private itemService: ItemService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    if (typeof this.authService.getUserIdFromToken() === 'number') {
+      const sellerId: number = this.authService.getUserIdFromToken();
+      const input: IItemFormInput = this.itemForm.value;
+      const formattedForm: ICreateItem = {
+        ...input,
+        sellerId: sellerId,
+      };
+      this.itemService.addItem(formattedForm);
+      this.router.navigate(['/shop']);
+    }
   }
-
 }
