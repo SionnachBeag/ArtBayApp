cab58f39c1430df6ac55f8243be59301eb3b4250
Lilla Csapo <lantasire@gmail.com>
2022-08-22 20:23:16 +0100

    onClick() method added

--- frontend/src/app/shared/item/item.component.ts
+++ frontend/src/app/shared/item/item.component.ts
@@ -1,14 +1,40 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IBuyItemModel } from 'src/app/core/models/IBuyItemModel';
 import { IItemsOnSaleViewModel } from 'src/app/core/models/IItemsOnSaleViewModel';
+import { AuthService } from 'src/app/core/services/auth-service/auth.service';
+import { ItemService } from 'src/app/core/services/item-service/item.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss'],
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
   @Input() itemData!: IItemsOnSaleViewModel;
-  constructor() {}
+  @Input() isShow!: boolean;
+  constructor(
+    private authService: AuthService,
+    private itemService: ItemService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  onClick(): void {
+    if (typeof this.authService.getUserIdFromToken() === 'number') {
+      const buyerId: number = this.authService.getUserIdFromToken();
+      const buyerObj: IBuyItemModel = {
+        buyerId: buyerId,
+      };
+      this.itemService
+        .buyItem(this.itemData.id, buyerObj)
+        .subscribe((response) => {
+          if (response.status === 200) {
+            this.router.navigate(['/myItems'], {
+              queryParams: { id: buyerId },
+            });
+            console.log(response.message);
+          }
+        });
+    }
+  }
 }
