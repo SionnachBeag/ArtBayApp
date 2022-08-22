8336df374cc0603c30f4d14076738a84ed578ea0
Lilla Csapo <lantasire@gmail.com>
2022-08-22 16:37:46 +0100

    added modules to imports

--- frontend/src/app/modules/pages/manage-items-page/manage-items-page.module.ts
+++ frontend/src/app/modules/pages/manage-items-page/manage-items-page.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageItemsPageRoutingModule } from './manage-items-page-routing.module';
 import { ManageItemsPageComponent } from './manage-items-page.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [ManageItemsPageComponent],
-  imports: [CommonModule, ManageItemsPageRoutingModule],
+  imports: [
+    CommonModule,
+    ManageItemsPageRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class ManageItemsPageModule {}
