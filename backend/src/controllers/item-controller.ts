ff9a3563b2a0e4a043fc883f636d4875024a02b7
Lilla Csapo <lantasire@gmail.com>
2022-08-22 20:35:37 +0100

    buyItem() implemented

--- backend/src/controllers/item-controller.ts
+++ backend/src/controllers/item-controller.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { ApiErrorModel } from '../models/ApiErrorModel';
+import { IBuyItemRequestModel } from '../models/request-models/IBuyItemRequestModel';
 import { ICreateItemRequestModel } from '../models/request-models/ICreateItemRequestModel';
 import { ICreateItemViewModel } from '../models/view-models/ICreateItemViewModel';
 import { IItemByIdViewModel } from '../models/view-models/IItemByIdViewModel';
 import { IItemsOnSaleViewModel } from '../models/view-models/IItemsOnSaleViewModel';
+import { ISuccessViewModel } from '../models/view-models/ISuccessViewModel';
 import { imgUrlService } from '../services/img-url-service';
 import { itemService } from '../services/item-service';
 
@@ -89,6 +91,28 @@ export const itemController = {
         return res.json(data);
       })
       .catch((err: ApiErrorModel) => {
+        console.log(err);
+        next(err);
+        return;
+      });
+  },
+
+  async buyItem(
+    req: Request,
+    res: Response<ISuccessViewModel>,
+    next: NextFunction
+  ) {
+    const { buyerId }: IBuyItemRequestModel = req.body;
+    const { id } = <{ id: string }>req.params;
+    await itemService
+      .buyItem(id, buyerId)
+      .then(() => {
+        return res.json({
+          status: 200,
+          message: 'Item is sold.',
+        });
+      })
+      .catch((err) => {
         console.log(err);
         next(err);
         return;
