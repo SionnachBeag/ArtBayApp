95cbe2a7dfcbe6f4d011bccf6ffa8b18772ac572
Lilla Csapo <lantasire@gmail.com>
2022-08-22 10:51:44 +0100

    getItemById modified

--- backend/src/services/item-service.ts
+++ backend/src/services/item-service.ts
@@ -1,4 +1,7 @@
+import { IBuyerDomainModel } from '../models/domain-models/IBuyerDomainModel';
+import { IItemByIdDomainModel } from '../models/domain-models/IItemByIdDomainModel';
 import { IItemDomainModel } from '../models/domain-models/IItemDomainModel';
+import { IItemAndBuyerModel } from '../models/IItemAndBuyerModel';
 import { ICreateItemRequestModel } from '../models/request-models/ICreateItemRequestModel';
 import { itemRepository } from '../repository/item-repository';
 
@@ -11,13 +14,30 @@ export const itemService = {
     return await itemRepository.listAllItemsOnSale();
   },
 
-  async getItemById(id: string): Promise<IItemDomainModel> {
-    const itemById = await itemRepository.getItemById(id);
+  async getItemById(
+    id: string
+  ): Promise<IItemByIdDomainModel | IItemAndBuyerModel> {
+    let itemById = await itemRepository.getItemById(id);
     if (!itemById) {
       return Promise.reject({
         message: 'item is not found',
         status: 404,
       });
+    }
+
+    if (itemById.isSold) {
+      const buyerName: IBuyerDomainModel = await itemRepository.getBuyerName(
+        id
+      );
+      const addBuyerName = (
+        input: IItemByIdDomainModel
+      ): IItemAndBuyerModel => {
+        return {
+          ...input,
+          buyerName: buyerName.userName,
+        };
+      };
+      itemById = addBuyerName(itemById);
     }
     return itemById;
   },
