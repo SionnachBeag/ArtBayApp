1e37de043e782ca92957a04397e4340f406684f1
Lilla Csapo <lantasire@gmail.com>
2022-08-22 16:40:36 +0100

    addItem and listItemsBought methods added

--- frontend/src/app/core/services/item-service/item.service.ts
+++ frontend/src/app/core/services/item-service/item.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ICreateItem } from '../../models/ICreateItem';
+import { ICreateItemViewModel } from '../../models/ICreateItemViewModel';
 import { IItemByIdViewModel } from '../../models/IItemByIdViewModel';
 import { IItemsOnSaleViewModel } from '../../models/IItemsOnSaleViewModel';
 
@@ -14,12 +16,33 @@ export class ItemService {
     this.itemsSubject.asObservable();
 
   constructor(private http: HttpClient) {}
-  listAllItemsOnSale() {
+  addItem(input: ICreateItem): void {
+    this.http
+      .post<ICreateItemViewModel>(`${environment.baseUrl}/items`, input)
+      .subscribe((response: ICreateItemViewModel) => {
+        const itemsSubject: IItemsOnSaleViewModel[] =
+          this.itemsSubject.getValue();
+        itemsSubject.push({
+          ...input,
+          isSold: false,
+          id: response.id,
+        });
+        this.itemsSubject.next(itemsSubject);
+      });
+  }
+
+  listAllItemsOnSale(): void {
     this.http
       .get<IItemsOnSaleViewModel[]>(`${environment.baseUrl}/items`)
       .subscribe((items: IItemsOnSaleViewModel[]) => {
         this.itemsSubject.next(items);
       });
+  }
+
+  listItemsBought(id: number): Observable<IItemsOnSaleViewModel[]> {
+    return this.http.get<IItemsOnSaleViewModel[]>(
+      `${environment.baseUrl}/myItems/${id}`
+    );
   }
 
   getItemById(id: number): Observable<IItemByIdViewModel> {
