557365967a898621b5f59f2a897797bbdb39705f
Lilla Csapo <lantasire@gmail.com>
2022-08-24 12:28:52 +0100

    created error handler interceptor test file

---
+++ frontend/src/app/core/interceptors/error-handler-interceptor/error-handler.interceptor.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { ErrorHandlerInterceptor } from './error-handler.interceptor';
+
+describe('ErrorHandlerInterceptor', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule],
+      providers: [ErrorHandlerInterceptor],
+    })
+  );
+
+  it('should be created', () => {
+    const interceptor: ErrorHandlerInterceptor = TestBed.inject(
+      ErrorHandlerInterceptor
+    );
+    expect(interceptor).toBeTruthy();
+  });
+});
