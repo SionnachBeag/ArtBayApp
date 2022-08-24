7fdaf67a6e849d2bb9f1cf704031249ce66642bf
Lilla Csapo <lantasire@gmail.com>
2022-08-24 11:12:12 +0100

    confirm popup component added to declarations

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -5,10 +5,17 @@ import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ItemComponent } from './item/item.component';
+import { ConfirmationPopupComponent } from './confirmation-popup/confirmation-popup.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [ItemComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule, MatButtonModule, RouterModule],
+  declarations: [
+    ItemComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConfirmationPopupComponent,
+  ],
+  imports: [CommonModule, MatButtonModule, RouterModule, MatDialogModule],
   exports: [ItemComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
