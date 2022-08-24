13ef9d7715c27bec9f71c493d922e4eb130a581a
Lilla Csapo <lantasire@gmail.com>
2022-08-24 12:28:43 +0100

    created error handler interceptor

---
+++ frontend/src/app/core/interceptors/error-handler-interceptor/error-handler.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpErrorResponse,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { SnackBarService } from '../../services/snackbar/snack-bar.service';
+
+@Injectable()
+export class ErrorHandlerInterceptor implements HttpInterceptor {
+  constructor(private snackBarService: SnackBarService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+        if (error.status === 400) {
+          this.snackBarService.showInfoMessage(error.error.message);
+        } else if (error.status > 400) {
+          this.snackBarService.showErrorMessage(error.message);
+        } else if (error.status === 307) {
+          return throwError(() => error);
+        }
+        return throwError(() => errorMsg);
+      })
+    );
+  }
+}
