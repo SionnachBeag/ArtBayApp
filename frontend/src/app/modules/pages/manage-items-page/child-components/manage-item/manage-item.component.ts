1bb63f987e9d42d85cf62b1579411c2d990ed2ca
Lilla Csapo <lantasire@gmail.com>
2022-08-24 11:12:31 +0100

    openDeleteModal() added

--- frontend/src/app/modules/pages/manage-items-page/child-components/manage-item/manage-item.component.ts
+++ frontend/src/app/modules/pages/manage-items-page/child-components/manage-item/manage-item.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { IItemsOnSaleViewModel } from 'src/app/core/models/IItemsOnSaleViewModel';
 import { ItemService } from 'src/app/core/services/item-service/item.service';
 import { EditPopupComponent } from '../edit-popup/edit-popup.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ConfirmationPopupComponent } from 'src/app/shared/confirmation-popup/confirmation-popup.component';
 
 @Component({
   selector: 'app-manage-item',
@@ -13,8 +14,18 @@ export class ManageItemComponent {
   @Input() itemData!: IItemsOnSaleViewModel;
   constructor(private itemService: ItemService, public dialog: MatDialog) {}
 
-  onDelete(): void {
-    this.itemService.deleteItemById(this.itemData.id);
+  openDeleteModal(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.panelClass = 'custom-dialog-container';
+    dialogConfig.data = {
+      id: this.itemData.id,
+      name: 'deleteItem',
+      question: 'Are you sure you want to delete this item?',
+      cancelButtonText: 'No',
+      actionButtonText: 'Yes',
+    };
+    this.dialog.open(ConfirmationPopupComponent, dialogConfig);
   }
 
   onEdit(): void {
